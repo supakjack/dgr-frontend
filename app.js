48881c36e726ff68d951db72dcaf5a579add67ec
unknown <48548611+supakjack@users.noreply.github.com>
2021-07-28 07:01:27 +0700

    add helper and fix debug
    add helper and fix debug

--- app.js
+++ app.js
@@ -28,174 +28,96 @@ if ($state.user.id) {
             localStorage.setItem("token", $state.user.token)
             localStorage.setItem("role", $state.user.role)
             localStorage.setItem("id", $state.user.id)
-
-            // sammy.js
-
-            console.log($state.user);
-
-            // manage page route
-            $.each(router, function (indexInArray, valueOfElement) {
-                const { path, file, role } = valueOfElement
-                $state.path = path
-                app.get('#/' + path, function () {
-                    $state.loading = loadingOverlay().activate();
-                    if ($state.user.role == 'admin' && path != 'home') {
-                        console.log("admin come in");
-                        if (path == 'auth') {
-                            window.location = '#/config-system'
-                            console.log("admin come to auth");
-                            loadingOverlay().cancel($state.loading);
-                            return
-                        } else if (role != 'admin' && role != 'public') {
-                            console.log("not admin permission");
-                            window.location = '#/home'
-                            console.log("go to admin home");
-                            loadingOverlay().cancel($state.loading);
-                            return
-                        }
-                        $('#header_public').hide();
-                        $('#header_staff').hide();
-                        $('#header_admin').show();
-                    } else if ($state.user.role == 'staff' && path != 'home') {
-                        console.log("staff come in");
-                        if (path == 'auth') {
-                            window.location = '#/library'
-                            console.log("staff come to auth");
-                            loadingOverlay().cancel($state.loading);
-                            return
-                        } else if (role != 'staff' && role != 'public') {
-                            console.log("not staff permission");
-                            window.location = '#/library'
-                            console.log("go to staff home");
-                            loadingOverlay().cancel($state.loading);
-                            return
-                        }
-                        $('#header_public').hide();
-                        $('#header_staff').show();
-                        $('#header_admin').hide();
-                    } else if (role != 'public') {
-                        console.log(role);
-                        console.log(role != 'public');
-                        console.log("not public path");
-                        if (role != $state.user.role) {
-                            console.log($state.user.role);
-                            window.location = '#/home'
-                            console.log("no permission");
-                            loadingOverlay().cancel($state.loading);
-                            return
-                        }
-                    }
-                    else if (path != 'home') {
-                        $('#header_public').show();
-                        $('#header_staff').hide();
-                        $('#header_admin').hide();
-                    } else {
-                        $('#header_public').hide();
-                        $('#header_staff').hide();
-                        $('#header_admin').hide();
-                    }
-                    console.log(this.params, path, file);
-                    this.$element().load('views/' + file + '.html');
-
-                });
-            });
-
-            app.notFound = function () {
-                $state.loading = loadingOverlay().activate();
-                $('#header_public').hide();
-                $('#header_staff').hide();
-                $('#header_admin').hide();
-                this.$element().load('views/home.html');
-            }
-
-            app.run();
-
-            console.log($state.path);
-            // sammy.js
         }
     });
-} else {
-    // sammy.js
-
-    console.log($state.user);
-
-    // manage page route
-    $.each(router, function (indexInArray, valueOfElement) {
-        const { path, file, role } = valueOfElement
-        $state.path = path
-        app.get('#/' + path, function () {
-            $state.loading = loadingOverlay().activate();
-            if ($state.user.role == 'admin' && path != 'home') {
-                console.log("admin come in");
-                if (path == 'auth') {
-                    window.location = '#/config-system'
-                    console.log("admin come to auth");
-                    loadingOverlay().cancel($state.loading);
-                    return
-                } else if (role != 'admin' && role != 'public') {
-                    console.log("not admin permission");
-                    window.location = '#/home'
-                    console.log("go to admin home");
-                    loadingOverlay().cancel($state.loading);
-                    return
-                }
-                $('#header_public').hide();
-                $('#header_staff').hide();
-                $('#header_admin').show();
-            } else if ($state.user.role == 'staff' && path != 'home') {
-                console.log("staff come in");
-                if (path == 'auth') {
-                    window.location = '#/library'
-                    console.log("staff come to auth");
-                    loadingOverlay().cancel($state.loading);
-                    return
-                } else if (role != 'staff' && role != 'public') {
-                    console.log("not staff permission");
-                    window.location = '#/library'
-                    console.log("go to staff home");
-                    loadingOverlay().cancel($state.loading);
-                    return
-                }
-                $('#header_public').hide();
-                $('#header_staff').show();
-                $('#header_admin').hide();
-            } else if (role != 'public') {
-                console.log(role);
-                console.log(role != 'public');
-                console.log("not public path");
-                if (role != $state.user.role) {
-                    console.log($state.user.role);
-                    window.location = '#/home'
-                    console.log("no permission");
-                    loadingOverlay().cancel($state.loading);
-                    return
-                }
-            }
-            else if (path != 'home') {
-                $('#header_public').show();
-                $('#header_staff').hide();
-                $('#header_admin').hide();
-            } else {
-                $('#header_public').hide();
-                $('#header_staff').hide();
-                $('#header_admin').hide();
-            }
-            console.log(this.params, path, file);
-            this.$element().load('views/' + file + '.html');
-
-        });
-    });
-
-    app.notFound = function () {
-        $state.loading = loadingOverlay().activate();
-        $('#header_public').hide();
-        $('#header_staff').hide();
-        $('#header_admin').hide();
-        this.$element().load('views/home.html');
-    }
-
-    app.run();
-
-    console.log($state.path);
-    // sammy.js
 }
+
+console.log($state.user);
+
+// manage page route
+$.each(router, function (indexInArray, valueOfElement) {
+    const { path, file, role } = valueOfElement
+    $state.path = path
+    app.get('#/' + path, function () {
+
+        if ($state.user.role == 'admin' && path != 'home') {
+            console.log("admin come in");
+            if (path == 'auth') {
+                window.location = '#/config-system'
+                console.log("admin come to auth");
+
+                return
+            } else if (role != 'admin' && role != 'public') {
+                console.log("not admin permission");
+                window.location = '#/home'
+                console.log("go to admin home");
+
+                return
+            }
+            $('#header_public').hide();
+            $('#header_staff').hide();
+            $('#header_admin').show();
+
+
+        } else if ($state.user.role == 'staff' && path != 'home') {
+            console.log("staff come in");
+            if (path == 'auth') {
+                window.location = '#/library'
+                console.log("staff come to auth");
+
+                return
+            } else if (role != 'staff' && role != 'public') {
+                console.log("not staff permission");
+                window.location = '#/library'
+                console.log("go to staff home");
+
+                return
+            }
+            $('#header_public').hide();
+            $('#header_staff').show();
+            $('#header_admin').hide();
+
+
+        } else if (role != 'public') {
+            console.log(role);
+            console.log(role != 'public');
+            console.log("not public path");
+            if (role != $state.user.role) {
+                console.log($state.user.role);
+                window.location = '#/home'
+                console.log("no permission");
+
+                return
+            }
+
+
+        }
+        else if (path != 'home') {
+            $('#header_public').show();
+            $('#header_staff').hide();
+            $('#header_admin').hide();
+            loadingOverlay().cancel($state.loading);
+
+        } else {
+            $('#header_public').hide();
+            $('#header_staff').hide();
+            $('#header_admin').hide();
+
+
+        }
+        console.log(this.params, path, file);
+        this.$element().load('views/' + file + '.html');
+    });
+});
+
+app.notFound = function () {
+    $state.loading = loadingOverlay().activate();
+    $('#header_public').hide();
+    $('#header_staff').hide();
+    $('#header_admin').hide();
+    this.$element().load('views/home.html');
+}
+
+app.run();
+
+console.log($state.path);
